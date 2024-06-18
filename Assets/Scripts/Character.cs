86ea061e0a3501e5e77fdc0191b39fb69d466352
Imnyeong <lmh4873@gmail.com>
2024-06-18 12:16:08 +0900

    feat: 캐릭터 이동 구현

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private Vector2 inputVector;
+    private float speed = 5.0f;
+
+    private Rigidbody2D rigid;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    #region Unity Life Cycle
+    private void Awake()
+    {
+        Init();
+    }
+    private void Update()
+    {
+        CheckInput();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void LateUpdate()
+    {
+        CheckFlip();
+        ChaeckAnimation();
+    }
+    #endregion
+
+    private void Init()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+    private void CheckInput()
+    {
+        inputVector.x = Input.GetAxisRaw("Horizontal");
+        inputVector.y = Input.GetAxisRaw("Vertical");
+    }
+    private void CheckFlip()
+    {
+        if (inputVector.x == 0)
+            return;
+        spriteRenderer.flipX = inputVector.x < 0;
+    }
+    private void ChaeckAnimation()
+    {
+        animator.SetBool("Move", inputVector.magnitude != 0);
+    }
+    private void Move()
+    {
+        Vector2 moveVector = inputVector.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + moveVector);
+    }
+}
